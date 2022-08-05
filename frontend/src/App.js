680e7250516303e3791fde2e48a2bbb96a4b0ed0
Blockchain8318 <suraj.prajapati@mail.vinove.com>
2022-08-05 15:24:39 +0530

    shopping

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href='/'>Shopping_cart</a>
+      </header>
+      <main>
+        list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
