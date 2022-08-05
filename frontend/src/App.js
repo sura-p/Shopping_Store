f108bfd7eae0e38582de8d47b709d56235a256ee
Blockchain8318 <suraj.prajapati@mail.vinove.com>
2022-08-05 17:14:02 +0530

    shopping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
+import {Link} from 'react-router-dom'
 function App() {
+  console.log(data);
   return (
     <div>
-      <header >
-        <a href='/'>Shopping_cart</a>
+      <header>
+        <a href="/">Shopping_cart</a>
       </header>
       <main>
-        list product
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              
+              <img src={product.image} alt={product.name} />
+              
+              <div className="product-info">
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+
+              <button>Add to cart</button>
+              </div>
+              
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,45 @@
+const data = {
+    products:[
+
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 129,
+            countInStock:10,
+            brand:"Nike",
+            rating:4.2,
+            numReviews:10,
+            description: 'high quality product'
+        }
+,
+        {
+            name:'Adidas Slim Shirt',
+            slug:'adidas-slim-shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price: 150,
+            countInStock:10,
+            brand:"Adidas",
+            rating:4.3,
+            numReviews:10,
+            description: 'high quality product'
+        }
+
+,
+        {
+            name:'Nike Fit Pant',
+            slug:'nike-fit-Pant',
+            category:'Pants',
+            image:'/images/p3.jpg',
+            price: 200,
+            countInStock:10,
+            brand:"Nike",
+            rating:4.2,
+            numReviews:10,
+            description: 'high quality product'
+        }
+    ]
+};
+export default data;
